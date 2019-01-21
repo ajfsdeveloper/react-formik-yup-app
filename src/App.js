dd0d659098932a53991bf7429d4aefa0a7dac6b2
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-01-21 23:22:44 +0530

    Add checkbox and select components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import { withFormik, Form, Field } from 'formik'
 
-const App = () => (
+const App = ({ values }) => (
   <Form>
     <Field type="email" name="email" placeholder="Email" />
     <Field type="password" name="password" placeholder="Password" />
+    <label>
+      <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+      Join our newsletter
+    </label>
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
     <button type="submit">Submit</button>
   </Form>
 )
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || '',
-      password: password || ''
+      password: password || '',
+      newsletter: newsletter || true,
+      plan: plan || 'free'
     }
   },
   handleSubmit(values) {
