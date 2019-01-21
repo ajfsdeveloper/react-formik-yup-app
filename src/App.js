491360b1cd674e1bdab1670a24c20e558f234be9
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-01-21 23:06:10 +0530

    Add Formik to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import { withFormik } from 'formik'
 
-const App = () => <div>App Test!</div>
+const App = ({ values, handleChange }) => (
+  <div>
+    <input
+      type="email"
+      name="email"
+      placeholder="Email"
+      value={values.email}
+      onChange={handleChange}
+    />
+  </div>
+)
 
-export default App
+const FormikApp = withFormik({
+  mapPropsToValues() {
+    return {
+      email: 'test email'
+    }
+  }
+})(App)
+
+export default FormikApp
